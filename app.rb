903a23684d7ba1037c9e59b2ad1b58abb9e7cfcc
Jason Knebel <jasonkneb@gmail.com>
2012-12-20 23:23:15 -0600

    adding heroku support
    trying to get livereload with heroku support

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 
-require 'rack-livereload'
+require 'rack-livereload' if development?
 require 'sinatra/reloader' if development?
-use Rack::LiveReload
+use Rack::LiveReload if development?
 
 get '/' do
   "hello, world"
