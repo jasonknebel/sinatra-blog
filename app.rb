67bedcca4069b1d6b3df790fde330f70478127a0
Jason Knebel <jasonkneb@gmail.com>
2012-12-21 08:16:24 -0500

    abandoning Sass for now; setup complete

--- app.rb
+++ app.rb
@@ -2,23 +2,24 @@ require 'sinatra'
 require 'slim'
 require 'sass'
 
+
+
 if development?
   require 'rack-livereload'
   require 'sinatra/reloader'
   use Rack::LiveReload
 end
 
-class App < Sinatra::Base
 
-  get '/style.css' do
-    sass style.to_sym
-  end
 
-  get '/' do
-    slim :index
-  end
-
-  if __FILE__ == $0
-    App.run! :port => 4000
-  end
+get '/' do
+  slim :index
 end
+
+
+
+
+# get '/:style.css' do |style|
+#   content_type 'text/css'
+#   sass style.to_sym
+# end
