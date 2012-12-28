44ab4e58a215fe5efe33e0cb09c7f98b37746528
Jason Knebel <jasonkneb@gmail.com>
2012-12-28 13:12:28 -0500

    adding config :production back

--- app.rb
+++ app.rb
@@ -14,23 +14,27 @@ end
 use Rack::MethodOverride
 
 configure :development do
+  set :database, 'sqlite3:///db/blog_development.sqlite3'
   require 'rack-livereload'
   require 'sinatra/reloader'
   use Rack::LiveReload
 end
 
-db = URI.parse(ENV['HEROKU_POSTGRESQL_JADE_URL'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :port     => db.port,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'unicode',
-  :pool => 5
-)
+configure :production do
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_JADE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'unicode',
+    :pool => 5
+  )
+end
 
 #--------------------Helpers--------------------#
 
